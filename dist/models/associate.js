55c6518d060c3e99dd33fb56d2775d556974a1fa
mạnh bùi <manhhn.nv5@gmail.com>
2022-10-18 16:47:17 +0700

    create constant variable

--- dist/models/associate.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.associate = void 0;
-function associate(sequelizeConnection) {
-    sequelizeConnection.sync({ force: true });
-}
-exports.associate = associate;
