4e0ff168b296ff466e5b52192e3c5b3e25c44735
manhhoach <manhhn.nv5@gmail.com>
2023-04-11 16:45:55 +0700

    upload to s3

--- src/api/helpers/handleFileName.ts
+++ src/api/helpers/handleFileName.ts
@@ -1,5 +1,6 @@
 export const handleFileName = (fileName: string) => {
     let str = fileName.slice(0, fileName.lastIndexOf('.'));
+    /* eslint-disable no-useless-escape */
     str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
     str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
     str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
@@ -28,4 +29,4 @@ export const handleFileName = (fileName: string) => {
     str = str.toLowerCase();
     str = str.replace(/ /g, '.');
     return str;
-}
+};
--- src/api/middlewares/upload.ts
+++ src/api/middlewares/upload.ts
@@ -2,6 +2,5 @@ const multer = require('multer');
 
 const storage = multer.memoryStorage();
 
-//@typescript-eslint/no-var-requires
 export const uploadMulter = multer({ storage: storage }); 
 
