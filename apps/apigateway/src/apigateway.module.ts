c4e97a2210728757f3b0ee27b49a6942c24ed960
manhhoach <manhhn.nv5@gmail.com>
2023-08-05 16:50:18 +0700

    fix bug

--- apps/apigateway/src/apigateway.module.ts
+++ apps/apigateway/src/apigateway.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ApigatewayController } from './apigateway.controller';
-import { ApigatewayService } from './apigateway.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AuthModule],
-  controllers: [ApigatewayController],
-  providers: [ApigatewayService],
+  controllers: [],
+  providers: [],
 })
 export class ApigatewayModule {}
